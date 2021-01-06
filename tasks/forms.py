f059c3664ec20faf9cfd767c473c28f96a71a7e2
surojit sarkar <surojit@pankaj>
2021-01-06 11:51:44 +0530

    Third commit

--- tasks/forms.py
+++ tasks/forms.py
@@ -31,21 +31,17 @@ class UserRegisterationForm(ModelForm):
         model = MyUser
         fields = ['username','firstname','lastname','email','password','confirm_password','birthdate','phone','address']
 
-    '''def clean(self):
+    def clean(self):
         cleaned_data = super(UserRegisterationForm, self).clean()
         password = cleaned_data["password"]
         password2 = cleaned_data["confirm_password"]
-        print("***********Password clean***********")
-        print('P1:', password)
-        print('P2:', password2)
         if password != password2:
             raise forms.ValidationError("Password doesn't match")
-        return password'''
+        return password
     
 
     def clean_username(self):
         username = self.cleaned_data.get("username")
-        print("***********Username clean***********")
         if MyUser.objects.filter(username=username).exists():
             raise forms.ValidationError("This username is already taken")
         return username
@@ -53,9 +49,7 @@ class UserRegisterationForm(ModelForm):
     
     def clean_phone(self):
         phone = self.cleaned_data.get("phone")
-
         phone_validation = re.findall("\d", phone)
-        print("***********Phone clean***********")
         if MyUser.objects.filter(phone=phone).exists():
             raise forms.ValidationError("This phone number is already taken")
 
@@ -69,7 +63,6 @@ class UserRegisterationForm(ModelForm):
 
     def clean_birthdate(self):
         birthdate = self.cleaned_data.get("birthdate")
-        print("***********DOB clean***********")
         if birthdate > utc_now:
             raise forms.ValidationError("Date of Birth cannot be in future")
         return birthdate
@@ -78,7 +71,6 @@ class UserRegisterationForm(ModelForm):
     def clean_firstname(self):
         firstname = self.cleaned_data.get("firstname")
         lastname = self.cleaned_data.get("lastname")
-        print("***********Firstname clean***********")
         if firstname == lastname:
             raise ValidationError("Fisrtname and Lastname can't be same")
 
@@ -87,7 +79,6 @@ class UserRegisterationForm(ModelForm):
     
     def clean_email(self):
         email = self.cleaned_data.get("email")
-        print("***********Email clean***********")
         if len(email) > 6:
             if re.match('\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b', email) != None:
                 raise ValidationError("Email is not proper")
